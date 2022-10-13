3c05a66830e50b86fc8c50210de0bcb22a7144ab
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-13 13:21:51 +0900

    [update] Parser 가 문자열을 받고 split 하기 (쪼개기) 전에 작은 따옴표를 replaceAll 을 사용해 제거하도록 변경

--- src/main/java/DB/parser/HospitalParser.java
+++ src/main/java/DB/parser/HospitalParser.java
@@ -3,15 +3,11 @@ package DB.parser;
 import DB.domain.Hospital;
 
 public class HospitalParser implements ParseAccordingly<Hospital>{
-
-    private String replaceAllQuot(String str) {
-        return str.replaceAll("\'", "");
-    }
-
     @Override
     public Hospital parse(String toParse) {
+        toParse = toParse.replaceAll("\'", "");
         String[] info = toParse.split(",");
-        return new Hospital(replaceAllQuot(info[0]), replaceAllQuot(info[1]), replaceAllQuot(info[3]), replaceAllQuot(info[4]), replaceAllQuot(info[5]));
+        return new Hospital(info[0], info[1], info[3], info[4], info[5]);
     }
 
 }
