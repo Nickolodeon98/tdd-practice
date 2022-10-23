d3c493c4f75954f77a46c8212921d1beba8838ee
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-23 23:23:03 +0900

    [new] 코드업 4624번 문제 풀이 시작

---
+++ src/main/java/algorithms/BracketsValue.java
@@ -0,0 +1,37 @@
+package algorithms;
+
+import java.util.Stack;
+
+public class BracketsValue {
+    public int bracketsCheck(String bracketStr) {
+        Stack<String> bracketSet = new Stack<>();
+        int i = 0;
+        while (i < bracketStr.length()) {
+            bracketSet.add(Character.toString(bracketStr.charAt(i)));
+            i++;
+        }
+        int resultValue = 0;
+        String top = "";
+        top = bracketSet.pop();
+        if (top.equals("(") || top.equals("[")) {
+            return 0;
+        } else {
+            while (!bracketSet.empty()) {
+                if (top.equals(")")) {
+                    resultValue += 2;
+                    if ((top = bracketSet.pop()).equals("(")) top = bracketSet.pop();
+                }
+                else if (top.equals("]")) resultValue += 3;
+//                else if (top.equals)
+            }
+        }
+
+        if (bracketStr.equals("()")) {
+            return 2;
+        }
+        if (bracketStr.equals("[]")) {
+            return 3;
+        }
+        return 0; // if not correct
+    }
+}
