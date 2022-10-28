afa769a7596949b09fbfec39168fce05971f97f5
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-28 10:50:22 +0900

    fix: 프로그래머스 전화번호 목록 같은 전화번호이면 경우를 셀 때 제외되므로 if 문에서 필터링하도록 변경

--- src/main/java/algorithms/hash/programmers/P42577.java
+++ src/main/java/algorithms/hash/programmers/P42577.java
@@ -4,16 +4,19 @@ import java.util.HashSet;
 
 public class P42577 {
     public boolean solution(String[] phone_book) {
-        HashSet<String> hashBook = new HashSet<>();
+        public boolean solution(String[] phone_book) {
+            HashSet<String> hashBook = new HashSet<>();
 
-        for (var s : phone_book) hashBook.add(s);
-
-        /*최초 알고리즘*/
-        for (int i = 0; i < phone_book.length; i++) {
-            for (int j = i+1; j < phone_book.length; j++) {
-                if (phone_book[i].indexOf(phone_book[j])) return true;
+            for (var s : phone_book) hashBook.add(s);
+            boolean answer = true;
+            /*최초 알고리즘*/
+            for (int i = 0; i < phone_book.length; i++) {
+                for (int j = 0; j < phone_book.length; j++) {
+                    // System.out.println(phone_book[i]);
+                    // System.out.println(phone_book[j]);
+                    if (!phone_book[i].equals(phone_book[j]) && phone_book[i].indexOf(phone_book[j]) == 0) answer = false;
+                }
             }
+            return answer;
         }
-        return false;
-    }
 }
