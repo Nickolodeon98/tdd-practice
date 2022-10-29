028fea056a61e4b5b51fea4bb7b5efd25d8cba0d
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-29 23:50:01 +0900

    fix: 프로그래머스 전화번호 목록 풀이 완료

--- src/main/java/algorithms/hash/programmers/P42577.java
+++ src/main/java/algorithms/hash/programmers/P42577.java
@@ -4,19 +4,19 @@ import java.util.HashSet;
 
 public class P42577 {
     public boolean solution(String[] phone_book) {
-        public boolean solution(String[] phone_book) {
-            HashSet<String> hashBook = new HashSet<>();
+        HashSet<String> hashBook = new HashSet<>();
 
-            for (var s : phone_book) hashBook.add(s);
-            boolean answer = true;
-            /*최초 알고리즘*/
-            for (int i = 0; i < phone_book.length; i++) {
-                for (int j = 0; j < phone_book.length; j++) {
-                    // System.out.println(phone_book[i]);
-                    // System.out.println(phone_book[j]);
-                    if (!phone_book[i].equals(phone_book[j]) && phone_book[i].indexOf(phone_book[j]) == 0) answer = false;
-                }
+        for (var s : phone_book) hashBook.add(s);
+        boolean answer = true;
+
+        /*최초 알고리즘*/
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book[i].length(); j++) {
+                if (hashBook.contains(phone_book[i].substring(0, j))) return answer = false;
             }
-            return answer;
+
         }
+        return answer;
+    }
 }
+
