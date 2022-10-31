ee272da342752b2c237ccea9a6256e9f763efd82
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-31 10:12:13 +0900

    fix: 프로그래머스 모의고사 문제 배열에 저장하도록 변경

--- src/main/java/algorithms/exhaustivesearch/P42840.java
+++ src/main/java/algorithms/exhaustivesearch/P42840.java
@@ -7,22 +7,36 @@ package algorithms.exhaustivesearch;
 * 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
 * */
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class P42840 {
     public int[] solution(int[] answers) {
         int[] first = {1, 2, 3, 4, 5};
         int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        int[] answer = new int[3];
+        int[] cntInfo = new int[3];
 
-        int firstCorrectCnt = 0;
+        int max = 0;
 
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == first[i % first.length]) firstCorrectCnt++;
+            if (answers[i] == first[i % first.length]) {
+                cntInfo[0]++;
+                if (max < cntInfo[0]) max = cntInfo[0];
+            }
+            if (answers[i] == second[i % first.length]) {
+                cntInfo[1]++;
+                if (max < cntInfo[1]) max = cntInfo[1];
+            }
+            if (answers[i] == third[i % first.length]) {
+                cntInfo[2]++;
+                if (max < cntInfo[2]) max = cntInfo[2];
+            }
         }
-        answer[0] = firstCorrectCnt;
-        System.out.println(firstCorrectCnt);
-        return answer;
+
+        System.out.println(cntInfo[0]);
+        return cntInfo;
     }
 
     public static void main(String[] args) {
