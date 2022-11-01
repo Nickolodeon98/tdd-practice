c4697f55d0ef66d9bdd661630fa72276430938e6
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-01 10:14:24 +0900

    Feat: "프로그래머스 소수 찾기 문제 템플릿 콜백 메소드 적용"

--- src/main/java/algorithms/primenumber/P42839.java
+++ src/main/java/algorithms/primenumber/P42839.java
@@ -1,18 +1,35 @@
 package algorithms.primenumber;
 
 public class P42839 {
+
+    interface DividerMethod {
+        boolean divider(int a, int b);
+    }
+    private DividerMethod dividerMethod;
+
+    public P42839() {
+    }
+
+    public P42839(DividerMethod dividerMethod) {
+        this.dividerMethod = dividerMethod;
+    }
+
     public static void main(String[] args) {
-        P42839 p42839 = new P42839();
-        System.out.println(p42839.solution(13));
-        System.out.println(p42839.solution(17));
-        System.out.println(p42839.solution(19));
-        System.out.println(p42839.solution(23));
+        P42839 p42839First = new P42839((a, b)-> a < b);
+        P42839 p42839Second = new P42839((a, b)-> a < (b/2));
+        P42839 p42839Third = new P42839((a, b)-> a < Math.sqrt(b));
+
+        System.out.println(p42839First.solution(35));
+        System.out.println(p42839Second.solution(17));
+        System.out.println(p42839Third.solution(19));
+        System.out.println(p42839First.solution(23));
     }
 
     public boolean solution(int num) {
-        for (int i = 2; i <= Math.sqrt(num); i++) {
+        for (int i = 2; dividerMethod.divider(i, num); i++) {
             if (num % i == 0) return false; // 나누어 떨어지게 하는 값이 하나라도 있으면 소수가 아님
         }
         return true; // 나누어 떨어지게 하는 값이 하나도 없으면 소수임
     }
+
 }
