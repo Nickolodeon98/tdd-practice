c3270093367b3afeeb332a3f8618e294dca7d3de
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-02 09:40:28 +0900

    Fix: removeIf() 사용해서 2를 제외한 2의 배수 제거하도록 변경

--- src/main/java/algorithms/primenumber/FindAllPrimeNumbers.java
+++ src/main/java/algorithms/primenumber/FindAllPrimeNumbers.java
@@ -10,11 +10,10 @@ public class FindAllPrimeNumbers {
         for (int i = 2; i <= N; i++) {
             numbersToN.add(i);
         }
-        for (int j = 0; j < numbersToN.size(); j++) {
-            if (numbersToN.get(j) != 2 && numbersToN.get(j) % 2 == 0) numbersToN.remove(j);
-        }
 
-        System.out.println(numbersToN + " " + numbersToN.size());
+        numbersToN.removeIf(elem -> elem != 2 && elem % 2 == 0);
+
+        System.out.println("원소: " + numbersToN + ", 개수: " + numbersToN.size());
     }
 
     public static void main(String[] args) {
