3110d9ad8bfbe45a504aab600d9ed3a813927d65
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-07 10:02:37 +0900

    Fix: 프로그래머스 나누어 떨어지는 숫자 배열 List 로 다시 구현

--- src/main/java/algorithms/hash/programmers/P12910.java
+++ src/main/java/algorithms/hash/programmers/P12910.java
@@ -5,18 +5,19 @@ import java.util.*;
 public class P12910 {
     public int[] divisibleNumbersArray(int[] arr, int divisor) {
         int[] answer = {};
-        Queue<Integer> moduloZero = new PriorityQueue<>();
+        List<Integer> moduloZero = new ArrayList<>();
         for (int i : arr) {
             if (i % divisor == 0) moduloZero.add(i);
         }
         if (moduloZero.size() == 0) moduloZero.add(-1);
+        Collections.sort(moduloZero);
         answer = moduloZero.stream().mapToInt(i->i).toArray();
         return answer;
     }
 
     public static void main(String[] args) {
         P12910 p12910 = new P12910();
-        int[] testArr = {5, 9, 7, 10};
-        System.out.println(Arrays.toString(p12910.divisibleNumbersArray(testArr, 5)));
+        int[] testArr = {2, 36, 1, 3};
+        System.out.println(Arrays.toString(p12910.divisibleNumbersArray(testArr, 1)));
     }
 }
