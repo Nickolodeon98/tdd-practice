18bd6a64d2a69140132b7ab2a72436882fc9d934
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-07 09:27:20 +0900

    Feat: 하샤드 수 구하기 문제 자릿수의 합 구하는 함수 divisibleNum() 구현

--- src/main/java/algorithms/hash/programmers/P12947.java
+++ src/main/java/algorithms/hash/programmers/P12947.java
@@ -8,12 +8,23 @@ package algorithms.hash.programmers;
  * 예 2) 329가 주어진다. 각 자릿수의 합은 3 + 2 + 9 = 14이다.
  * 과정을 풀어서 쓰면, 329 / 10 = 32 ... 9. 32 / 10 = 3... 2
  * -> 329 / 10 / 10 + 329 % 10 + 329 / 10 % 10 = 3 + 9 + 2 = 14.
- * 즉, 몫이 10 보다 작아질 때까지 10으로 나눈 후 그 몫 + 모듈로(%) 연산자로 각 단계에서 구한 나머지들 을 하면 자릿수의 합이다.
+ * 즉, 몫이 1일 때까지 10으로 나눈 후 그 몫 + 모듈로(%) 연산자로 각 단계에서 구한 나머지들을 더하면 자릿수의 합이다.
  * */
 
 public class P12947 {
-    public void divisibleNum(int x) {
+    public int divisibleNum(int x) {
+        int value = x;
+        int result = 0;
+        while (value > 0) {
+            result += value % 10;
+            value = value / 10;
+        }
+//        System.out.println(result);
+        return result;
+    }
 
-
+    public static void main(String[] args) {
+        P12947 p12947 = new P12947();
+        p12947.divisibleNum(1429);
     }
 }
