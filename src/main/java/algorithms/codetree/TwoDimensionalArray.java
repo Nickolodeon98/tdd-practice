67031620342b7ff031d649af7be67d3535ee3edd
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-10 09:15:02 +0900

    Feat: 코드트리 2차원 배열 문제 풀이 완료

--- src/main/java/algorithms/codetree/TwoDimensionalArray.java
+++ src/main/java/algorithms/codetree/TwoDimensionalArray.java
@@ -1,5 +1,32 @@
 package algorithms.codetree;
 
-public class TwoDimensionalArray {
+import java.util.*;
 
+public class TwoDimensionalArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int size = sc.nextInt();
+
+        int[][] twoDArray = new int[size][2];
+        for (int i = 0; i < size; i++) {
+            twoDArray[i][0] = sc.nextInt();
+            twoDArray[i][1] = sc.nextInt();
+        }
+
+        int[][] newArray = new int[num][num];
+        int cnt = 1;
+        for (int[] oneDArray : twoDArray) {
+            newArray[oneDArray[0]-1][oneDArray[1]-1] = cnt;
+            cnt++;
+        }
+
+        for (int[] resultElements : newArray) {
+            for (int l = 0; l < num; l++) {
+                System.out.print(resultElements[l] + " ");
+            }
+            System.out.println();
+        }
+
+    }
 }
