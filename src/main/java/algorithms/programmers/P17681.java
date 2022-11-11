b6beef74f5102e20f5bae6fe38b51f57dabd34fa
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-11 09:33:28 +0900

    feat: Add 10진수를 2진수로 변환하는 메서드 구현

--- src/main/java/algorithms/programmers/P17681.java
+++ src/main/java/algorithms/programmers/P17681.java
@@ -6,13 +6,37 @@ public class P17681 {
         /* 1. 각 원소를 이진수로 변환 */
         String binary1 = "";
         String binary2 = "";
+        String[] binaryArr1 = new String[n];
+        String[] binaryArr2 = new String[n];
         for (int i = 0; i < n; i++) {
-            binary1 = Integer.toBinaryString(arr1[i]);
-            binary2 = Integer.toBinaryString(arr2[i]);
+            binaryArr1[i] = Integer.toBinaryString(arr1[i]);
+            binaryArr2[i] = Integer.toBinaryString(arr2[i]);
         }
 
-        /* 2. 2차원 배열에 넣을 문자열을 구성 */
+        /* 2. 배열에 넣을 문자열 구성 */
+        String secretCode = "";
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < binaryArr1[j].length(); k++) {
+                if (binaryArr1[j].charAt(k) == '1' || binaryArr2[j].charAt(k) == '1')
+                    secretCode = String.join("", secretCode, "#");
+            }
+        }
 
         return answer;
+    }
+
+    public String toBinaryNumberString(int decimalNum) {
+        String binary = "";
+        while (decimalNum > 1) {
+            binary = String.join("", Integer.toString(decimalNum % 2), binary);
+            decimalNum /= 2;
+        }
+        binary = String.join("", String.valueOf(decimalNum % 2), binary);
+        return binary;
+    }
+
+    public static void main(String[] args) {
+        P17681 p17681 = new P17681();
+        System.out.println(p17681.toBinaryNumberString(39));
     }
 }
