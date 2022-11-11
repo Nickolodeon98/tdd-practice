72762694bf8ead48e4adbb8c37f19bb1fa52dc9c
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-11 10:22:15 +0900

    feat: 프로그래머스 비밀 지도 리팩토링 2: 메서드에 태운 후 이진수로 변환하여 배열에 저장할 필요 없이 바로 문자열 생성 후 재귀 호출

--- src/main/java/algorithms/programmers/P17681.java
+++ src/main/java/algorithms/programmers/P17681.java
@@ -2,30 +2,32 @@ package algorithms.programmers;
 
 import java.util.Arrays;
 
+
+
+
 public class P17681 {
     public String[] solution(int n, int[] arr1, int[] arr2) {
+        int INITIAL_INDEX = 0;
         String[] answer = new String[n];
-        /* 1. 각 원소를 이진수로 변환 */
-        String[] binaryArr1 = new String[n];
-        String[] binaryArr2 = new String[n];
-        for (int i = 0; i < n; i++) {
-            binaryArr1[i] = toBinaryNumberString(arr1[i], n);
-            binaryArr2[i] = toBinaryNumberString(arr2[i], n);
-        }
 
-        /* 2. 배열에 넣을 문자열 구성 */
+        return recursiveGridBuilder(n, arr1, arr2, INITIAL_INDEX, answer);
+    }
+
+    public String[] recursiveGridBuilder(int n, int[] arr1, int[] arr2, int current, String[] answer) {
+        /* 배열에 넣을 문자열 구성 */
         String secretCode = "";
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < n; k++) {
-                if (binaryArr1[j].charAt(k) == '1' || binaryArr2[j].charAt(k) == '1')
-                    secretCode = String.join("", secretCode, "#");
-                else secretCode = String.join("", secretCode, " ");
-            }
-            answer[j] = secretCode;
-            secretCode = "";
+        if (current == n) return answer;
+
+        for (int k = 0; k < n; k++) {
+            /* 현재 가리키고 있는 인덱스의 원소를 2진수로 변환 */
+            if (toBinaryNumberString(arr1[current], n).charAt(k) == '1' || toBinaryNumberString(arr2[current], n).charAt(k) == '1')
+                secretCode = String.join("", secretCode, "#");
+            else secretCode = String.join("", secretCode, " ");
         }
-        System.out.println(Arrays.toString(answer));
-        return answer;
+        answer[current] = secretCode;
+
+        /* 재귀 호출 */
+        return recursiveGridBuilder(n, arr1, arr2, ++current, answer);
     }
 
     public String toBinaryNumberString(int decimalNum, int n) {
@@ -46,6 +48,8 @@ public class P17681 {
         P17681 p17681 = new P17681();
         int[] testArr1 = {46, 33, 33 ,22, 31, 50};
         int[] testArr2 = {27 ,56, 19, 14, 14, 10};
-        p17681.solution(6, testArr1, testArr2);
+        String[] ans = p17681.solution(6, testArr1, testArr2);
+
+        System.out.println(Arrays.toString(ans));
     }
 }
