2e11b0c94291a8289dff76653e01c9e352727a68
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-17 09:39:58 +0900

    feat: 재귀 이전까지의 로직 구현 중

--- src/main/java/algorithms/quicksort/QuickSortWithArray.java
+++ src/main/java/algorithms/quicksort/QuickSortWithArray.java
@@ -46,6 +46,36 @@ public class QuickSortWithArray {
         System.out.println(Arrays.toString(arr));
     }
 
+    public int[] sort(int startIdx, int endIdx) { // 최초의 start 와 end 는 각각 arr 의 가장 첫 인덱스와 가장 마지막 인덱스이다.
+        int oldPivotIdx = arr.length / 2; // 지금까지의 기준값 인덱스 선정 방식
+        int pivotIdx = (endIdx - startIdx + 1) / 2; // 변경된 선정 방식
+
+        int pivot = arr[pivotIdx]; // pivot 값 선정
+
+        /* 우선 편의를 위해 식별이 쉬운 이름을 가진 변수에 할당한다. */
+        int leftIdx = startIdx;
+        int rightIdx = endIdx;
+
+        while (rightIdx >= leftIdx) { // 두 지점이 교차될 때까지 반복한다.
+            if (arr[leftIdx] < pivot) { // pivot 보다 왼쪽에 있는 값이 자기 자리에 잘 있을 때
+                leftIdx++;
+                continue;
+            }
+            if (arr[rightIdx] > pivot) { // 왼쪽에 있는 값이 교환이 필요한데, pivot 보다 오른쪽에 있는 값이 자기 자리에 잘 있을 때
+                rightIdx--; // 새로운 교환 대상을 찾기 위해 한 칸 왼쪽으로 움직여 탐색을 이어나간다.
+                continue;
+            }
+            /* 만약 위 두 개의 조건문을 통과하고 여기까지 왔으면, 교환을 해야 되는 상황에 적절한 교환 대상도 찾았다는 뜻이다. */
+            swap(leftIdx, rightIdx); // 교환해주자
+            /* 교환 후에는 이번 phase 에서는 자기 자리를 찾아갔다. 이 자리 이후 자리들을 교환하자. */
+            leftIdx++;
+            rightIdx--;
+        }
+        /* 여기로 나왔을 떄는 이미 교차되어서 rightIdx < leftIdx 인 상황이다. */
+
+    }
+
+
 
     public static void main(String[] args) {
         int[] testArr = {20, 18, 5, 19, 40, 50, 5, 25};
