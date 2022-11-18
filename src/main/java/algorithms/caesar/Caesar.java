2a7eb4889eea44c4a9f8b0c3ee58dbff556ca36a
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-18 10:04:46 +0900

    feat: 프로그래머스 시저 암호 문제 공백을 포함하지 않는 문자열에 대해 구현 완료

--- src/main/java/algorithms/caesar/Caesar.java
+++ src/main/java/algorithms/caesar/Caesar.java
@@ -1,23 +1,6 @@
 package algorithms.caesar;
 
 public class Caesar {
-//    public String solution(String s, int n) {
-//        String answer = "";
-//        StringBuilder sb = new StringBuilder();
-//        int count = 1;
-//        for (int i = 0; i < s.length(); i++) {
-//            while (count <= n) {
-//                int codedNum = s.charAt(i) + 1;
-//                if (codedNum > 122) codedNum -= 25;
-//                if (codedNum > 90 && codedNum < 97) codedNum -= 25;
-//                char coded = (char) codedNum;
-//                sb.append(coded);
-//                count++;
-//            }
-//        }
-//        return answer;
-//    }
-
     /* 우선 하나의 문자가 주어졌을 때 밀 수 있는 알고리즘을 구현한다. */
     public char pullBehind(char c, int n) {
         int pulled = c + n;
@@ -29,9 +12,21 @@ public class Caesar {
         return (char) pulled;
     }
 
+    public String solution(String s, int n) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(pullBehind(s.charAt(i), n));
+        }
+        answer = sb.toString();
+        System.out.println(answer);
+        return answer;
+    }
+
     public static void main(String[] args) {
         Caesar caesar = new Caesar();
-
-        System.out.println(caesar.pullBehind('z', 3));
+        caesar.solution("ABc", 3); // DEf 가 나와야 함
+//        System.out.println(caesar.pullBehind('z', 3));
     }
 }
