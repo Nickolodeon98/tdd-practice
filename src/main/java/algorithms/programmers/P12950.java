8430e8822352c5acc57b2714444d0e64503aea11
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-21 23:04:09 +0900

    feat: 프로그래머스 행렬의 덧셈 재귀 호출 사용하도록 변경

--- src/main/java/algorithms/programmers/P12950.java
+++ src/main/java/algorithms/programmers/P12950.java
@@ -4,15 +4,16 @@ public class P12950 {
     /* 행렬은 2차원 배열로 표현한다.
      * 행은 2차원 배열 내에서 두 괄호 중 첫번째 값으로 참조한다.
      * 열은 괄호 중 두번째 값으로 참조한다. */
-    public int[][] solution(int[][] matrix1, int[][] matrix2) {
-        int[][] matrix = new int[matrix1.length][matrix1[0].length];
+    public int[][] solution(int[][] matrix1, int[][] matrix2, int current, int[][] matrix) {
+        int rowSize = matrix1.length;
+        if (current == rowSize) return matrix;
 
-        for (int i = 0; i < matrix1.length; i++) {
-            for (int j = 0; j < matrix1[i].length; j++) {
-                matrix[i][j] = matrix1[i][j] + matrix2[i][j];
-            }
+        int columnSize = matrix1[0].length;
+
+        for (int j = 0; j < columnSize; j++) {
+            matrix[current][j] = matrix1[current][j] + matrix2[current][j];
         }
-        return matrix;
+        return solution(matrix1, matrix2, current+1, matrix);
     }
 
     /* 리팩토링 할 수 있을까? */
