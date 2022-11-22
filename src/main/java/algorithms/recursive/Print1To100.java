2530c613193764c1e1ca901fd47c53d26c071a73
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-22 09:19:40 +0900

    feat: 재귀 호출을 이용해 1부터 100까지 출력하는 메서드 구현 완료

--- src/main/java/algorithms/recursive/Print1To100.java
+++ src/main/java/algorithms/recursive/Print1To100.java
@@ -2,12 +2,13 @@ package algorithms.recursive;
 
 public class Print1To100 {
     public void print(int i) {
-//        if (i > 100) return;
+        if (i > 100) return;
         try {
             System.out.println(i);
-            print(i);
-        } catch (StackOverflowError e) {
+            print(++i);
+        } catch (StackOverflowError e) { // 메시지가 한없이 출력되고 출력이 꼬이다가 어느 순간에 출력되기 때문에 한 줄에 문자열들이 서로 정해진 순서 없이 출력된다.
             System.out.println(e.getMessage());
+            System.out.println("스택 오버플로우 에러가 발생했습니다.");
 //            throw new RuntimeException(e);
         }
     }
