23bd911f80adc07f6b84d10def241855dad4f36e
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-24 11:01:55 +0900

    feat: 코드 업 별 계단 뛰어 오르기 풀이 완료

--- src/main/java/algorithms/codeup/Q1856.java
+++ src/main/java/algorithms/codeup/Q1856.java
@@ -26,22 +26,57 @@ public class Q1856 {
     }
 
     public int ascendStairs(int n, int stairs) {
-        if (n <= 0) return stairs-1;
+        int ways = ascendStairsWithOne(n, stairs);
+        if (n > 2) ways += ascendStairsWithTwo(n, stairs);
+        if (n > 3) ways += ascendStairsWithThree(n, stairs);
+        return ways;
+    }
+//    public int finalStairsClimbing(int n, int stairs) {
+//        if (n <= 0) return stairs;
+//        stairs++;
+//        int ways = finalStairsClimbing(n-1, stairs); // 중간 중간 ways 에 더해지는 불필요한 값들은 어떻게 처리하는가?
+//        ways += finalStairsClimbing(n-2, stairs= 0);
+//        ways += finalStairsClimbing(n-3, stairs= 0);
+//        return ways;
+//    }
+    public int ascendStairsWithOne(int n, int stairs) {
+        if (n == 0) return stairs;
+        if (n < 0) return stairs-1;
         /* n = 5 일 때,
          * ways = 4, 1
          * ways = 3, 2
          * ways = 2, 3
          * ways = 1, 4 */
-        int ways = ascendStairs(n-1, stairs + 1);
-
-        /* n = 5 일 때,
-         * */
-        if (n > 1) ways += ascendStairs(n-2, stairs + 1);
-
-        if (n > 2) ways += ascendStairs(n-3, stairs + 1);
-
-        return ways;
+        stairs++;
+        return ascendStairsWithOne(n-1, stairs); // ways 를 센다. 여기서 4로 가면서 stairs 는 1이 된다.
+                                                                // 현재 stairs 는 1인데 3으로 가면서 2가 된다.
     }
 
+    public int ascendStairsWithTwo(int n, int stairs) {
+        if (n == 0) return stairs;
+        if (n < 0) return stairs-1;
 
+        /* n = 5 일 때,
+         * ways = 1 + 1 + 1*/
+        stairs++;
+        return ascendStairsWithTwo(n-2, stairs); // 여기서 3으로 가면서 stairs 는 1이 된다.
+                                                                    // 현재 stairs 는 1인데 1로 가면서 2가 된다.
+                                                                        // ways = 2 로 리턴된다.
+    }
+
+    public int ascendStairsWithThree(int n, int stairs) {
+        if (n == 0) return stairs;
+        if (n < 0) return stairs-1;
+        /* n = 5 일 때,
+         * ways = 1 + 1 + 1*/
+        stairs++;
+        return ascendStairsWithThree(n-3, stairs); // 여기서 2 로 가면서 stairs 는 1이 된다.
+                                                                    // ways = 1 로 리턴된다.
+    }
+
+    public static void main(String[] args) {
+        Q1856 q1856 = new Q1856();
+        System.out.println(q1856.ascendStairs(2, 0));
+//        System.out.println(q1856.finalStairsClimbing(5, 0));
+    }
 }
