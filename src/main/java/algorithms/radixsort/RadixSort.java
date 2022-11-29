f9b58a306997a23f2cef3ddb06d7d2952980b355
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-29 10:25:27 +0900

    feat: 배열 각 숫자의 자릿수 구하는 메서드 구현 완료

--- src/main/java/algorithms/radixsort/RadixSort.java
+++ src/main/java/algorithms/radixsort/RadixSort.java
@@ -1,8 +1,6 @@
 package algorithms.radixsort;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class RadixSort {
     /* 숫자가 몇진수인지에 따라 생성하는 배열 내의 원소 개수가 정해진다.
@@ -25,9 +23,25 @@ public class RadixSort {
         return sorted.stream().mapToInt(i->i).toArray();
     }
 
+    /* 배열의 각 숫자의 자릿수 구하는 메서드 */
+    public int[] getDigits(int[] arr) {
+//        List<Integer> digits = new ArrayList<>();
+        HashSet<Integer> digits = new HashSet<>();
+        for (int num : arr) {
+            if (num == 0) {
+                digits.add(1);
+                continue;
+            }
+            int digit = (int)Math.log10(num)+1;
+            digits.add(digit);
+        }
+        return digits.stream().mapToInt(i->i).toArray();
+    }
+
     public static void main(String[] args) {
         RadixSort radixSort = new RadixSort();
-        int[] test = {7,4,5,9,1,0};
-        System.out.println(Arrays.toString(radixSort.forDecimals(test)));
+        int[] test = {0, 1, 2, 7, 7, 8, 13, 14, 16, 18, 203};
+//        System.out.println(Arrays.toString(radixSort.forDecimals(test)));
+        System.out.println(Arrays.toString(radixSort.getDigits(test)));
     }
 }
