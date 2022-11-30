0aa2f88e779624930af903aadd8d08f7b873660a
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-30 10:36:30 +0900

    docs: 노드 사이의 거리 풀이에 주석 추가

--- src/main/java/algorithms/binarytree/LowestCommonAncestor.java
+++ src/main/java/algorithms/binarytree/LowestCommonAncestor.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class LowestCommonAncestor {
     public int distBtwNodes(int a, int b) {
+        // 전지적 시점이다. 어디에서 시작해서 어디서 끝날지 정확히 알고 있기 때문에 조건문만 걸어서 카운트해주면 된다.
+        // 처음에 시작한 부분에서 끝 부분까지 어떤 경로로 이루어져 있는지 정확히 알고 있어서 이렇게 코딩이 가능하다.
         if (a > b) {
             int nodeALca = a / 2;
             return distBtwNodes(nodeALca, b) + 1;
