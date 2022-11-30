4292c129ff0f007f619c319559465d21ee49a229
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-30 10:36:04 +0900

    feat: 기수 정렬 구현 완료

--- src/main/java/algorithms/radixsort/RadixSort.java
+++ src/main/java/algorithms/radixsort/RadixSort.java
@@ -20,28 +20,50 @@ public class RadixSort {
             if (sortedElem != -1) sorted.add(sortedElem);
         }
 
-        return sorted.stream().mapToInt(i->i).toArray();
+        return sorted.stream().mapToInt(i -> i).toArray();
     }
 
     /* 배열의 각 숫자의 자릿수 구하는 메서드 */
     public int[] getDigits(int[] arr) {
-//        List<Integer> digits = new ArrayList<>();
         HashSet<Integer> digits = new HashSet<>();
         for (int num : arr) {
             if (num == 0) {
-                digits.add(1);
+                digits.add(0);
                 continue;
             }
-            int digit = (int)Math.log10(num)+1;
+            int digit = (int) Math.log10(num);
             digits.add(digit);
         }
-        return digits.stream().mapToInt(i->i).toArray();
+        int[] uniqueElements = digits.stream().mapToInt(i -> i).toArray();
+        Arrays.sort(uniqueElements);
+        return uniqueElements;
+    }
+
+    public int[] sort(int[] toSort, int[] digitTypes, int digitIdx) {
+        if (digitIdx == digitTypes.length) return toSort;
+        Queue<Integer>[] sortTool = new Queue[toSort.length];
+
+        for (int i = 0; i < sortTool.length; i++) {
+            sortTool[i] = new LinkedList<>();
+        }
+
+        for (int element : toSort) {
+            sortTool[Math.floorDiv(element, (int) Math.pow(10, digitTypes[digitIdx])) % 10].add(element);
+        }
+
+        List<Integer> sorted = new ArrayList<>();
+
+        for (Queue<Integer> queue : sortTool) {
+            sorted.addAll(queue);
+        }
+        return sort(sorted.stream().mapToInt(i->i).toArray(), digitTypes, ++digitIdx);
     }
 
     public static void main(String[] args) {
         RadixSort radixSort = new RadixSort();
-        int[] test = {0, 1, 2, 7, 7, 8, 13, 14, 16, 18, 203};
+        int[] test = {203, 0, 1, 2, 7, 7, 8, 13, 14, 16, 18};
 //        System.out.println(Arrays.toString(radixSort.forDecimals(test)));
-        System.out.println(Arrays.toString(radixSort.getDigits(test)));
+//        System.out.println(Arrays.toString(radixSort.getDigits(test)));
+        System.out.println(Arrays.toString(radixSort.sort(test, radixSort.getDigits(test), 0)));
     }
 }
