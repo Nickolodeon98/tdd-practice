1480a953087891419d0bf57558c3797da2b7d325
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-30 13:41:50 +0900

    feat: 모던 자바 문법 공부 - 스트림과 람다

---
+++ src/main/java/modern/Lecturer.java
@@ -0,0 +1,14 @@
+package modern;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Lecturer {
+    private String name;
+    private boolean isLikeAlgorithm;
+    private boolean isLikeSpringBoot;
+}
---
+++ src/test/java/modern/StudentTest.java
@@ -0,0 +1,44 @@
+package modern;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    List<Lecturer> lecturers;
+    Lecturer kyeongrok;
+    Lecturer kyeonghwan;
+    Lecturer sujin;
+    Lecturer sohyun;
+
+    @BeforeEach
+    void setUp() {
+        lecturers = new ArrayList<>();
+        kyeongrok = new Lecturer("김경록", true, true);
+        kyeonghwan = new Lecturer("고경환", true, false);
+        sujin = new Lecturer("김수진", false, true);
+        sohyun = new Lecturer("강소현", true, true);
+        lecturers.add(kyeongrok);
+        lecturers.add(kyeonghwan);
+        lecturers.add(sujin);
+        lecturers.add(sohyun);
+    }
+
+    @Test
+    void findCategory() {
+        List<String> likeAlgorithmLecturers = lecturers.stream()
+                .filter(Lecturer::isLikeAlgorithm)
+                .map(Lecturer::getName)
+                .collect(Collectors.toList());
+
+        for (String likeAlgorithmLecturer : likeAlgorithmLecturers) {
+            System.out.println(likeAlgorithmLecturer);
+        }
+    }
+}
