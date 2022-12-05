2e0c8ded1dff52bd6059908b09a881ac4fe3ed48
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-05 09:27:52 +0900

    feat: 행렬의 덧셈에 문제 대한 테스트 케이스 수정

--- src/test/java/algorithms/programmers/P12950Test.java
+++ src/test/java/algorithms/programmers/P12950Test.java
@@ -12,19 +12,22 @@ class P12950Test {
 
     int[][] matrix1;
     int[][] matrix2;
+    int[][] matrix;
 
     @BeforeEach
     void setUp() {
         p12950 = new P12950();
         matrix1 = new int[][]{{1,2},{2,3}};
         matrix2 = new int[][]{{3,4},{5,6}};
+        matrix = new int[matrix1.length][matrix1[0].length];
+
     }
 
     @Test
     @DisplayName("행렬의 덧셈을 정확하게 한다.")
     void addTwoMatrix() {
         int[][] expectedResult = new int[][]{{4,6},{7,9}};
-        int[][] actualResult = p12950.solution(matrix1, matrix2);
+        int[][] actualResult = p12950.solution(matrix1, matrix2, 0, matrix);
         for (int i = 0; i < expectedResult.length; i++) {
             for (int j = 0; j < expectedResult[i].length; j++) {
                 assertEquals(expectedResult[i][j], actualResult[i][j]);
