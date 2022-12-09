5fba8fd1adeed13d45b02d54e30f57af8d557b43
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-09 10:48:02 +0900

    feat: LCS 찾을 때 사용되는 표를 이중 for 문으로 작성

--- src/main/java/algorithms/dynamicprogramming/LCS.java
+++ src/main/java/algorithms/dynamicprogramming/LCS.java
@@ -1,7 +1,9 @@
 package algorithms.dynamicprogramming;
 
+import java.util.Arrays;
+
 public class LCS {
-    public String longestSubSequence() {
+    public void longestSubSequence() {
         String str1 = "ABCDCBA";
         String str2 = "DCABDC";
 
@@ -9,14 +11,27 @@ public class LCS {
          * 기준이 되는 문자열이 배열 안에 중첩되어서 들어가야 한다. 다른 문자열은 바깥에 들어가야 한다.
          * str1 을 기준으로 배열을 생성해보자. */
 
-        int[][] map = new int[str2.length()][str1.length()];
+        int[][] memo = new int[str2.length()+1][str1.length()+1];
 
-        for (int i = 0; i < str2.length(); i++) {
-            for (int j = 0; j < str1.length(); j++) {
-                System.out.printf("%s, %s\n", str2.charAt(i), str1.charAt(j));
+        for (int i = 1; i <= str2.length(); i++) {
+            for (int j = 1; j <= str1.length(); j++) {
+//                System.out.printf("%s, %s\n", str2.charAt(i), str1.charAt(j));
+
+                if (str2.charAt(i-1) == str1.charAt(j-1)) {
+                    memo[i][j] = 1 + memo[i - 1][j - 1];
+                    continue;
+                }
+                memo[i][j] = Math.max(memo[i][j-1], memo[i-1][j]);
             }
         }
-        return "";
+
+        for (int[] row : memo) {
+            System.out.println(Arrays.toString(row));
+        }
     }
 
+    public static void main(String[] args) {
+        LCS lcs = new LCS();
+        lcs.longestSubSequence();
+    }
 }
