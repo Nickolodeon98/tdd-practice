58b1c0323e8a6fcefb64d349c99727ca56dc78b1
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-13 10:27:44 +0900

    feat: 최적의 전략 알고리즘 숫자가 1개만 있을 경우와 2개만 있을 경우의 나와 상대방의 숫자 선택 결과를 gameBoard 에 저장

--- src/main/java/algorithms/dynamicprogramming/OptimalStrategy.java
+++ src/main/java/algorithms/dynamicprogramming/OptimalStrategy.java
@@ -1,15 +1,18 @@
 package algorithms.dynamicprogramming;
 
+import java.util.Arrays;
+
 public class OptimalStrategy {
 
     class Duple {
         int support;
         int enemy;
 
-        public Duple(int support, int enemy) {
-            this.support = support;
-            this.enemy = enemy;
+        public Duple() {
+            this.support = 0;
+            this.enemy = 0;
         }
+
 
         public void setSupport(int support) {
             this.support = support;
@@ -18,10 +21,25 @@ public class OptimalStrategy {
         public void setEnemy(int enemy) {
             this.enemy = enemy;
         }
+
+        @Override
+        public String toString() {
+            return "("+
+                    support + ", "
+                    + enemy +
+                    ")";
+        }
     }
 
     public void buildMatrix() {
         Duple[][] gameBoard = new Duple[4][4]; // Duple 네 개로 이루어진 행이 네 개가 있다. 아군과 적군의 점수가 적힌 게임 점수판을 나타낸다.
+
+        /* 향상된 for 문은 set 능력은 결여되어 있다. get 능력만 갖고 있기 때문에 배열을 초기화할 때는 적당한 반복문 형식이 아니다. */
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[i].length; j++) {
+                gameBoard[i][j] = new Duple();
+            }
+        }
 
         int[] columns = {2, 7, 40, 19};
 
@@ -38,5 +56,13 @@ public class OptimalStrategy {
             }
         }
 
+        for (Duple[] dupleGroup : gameBoard) {
+            System.out.println(Arrays.toString(dupleGroup));
+        }
+    }
+
+    public static void main(String[] args) {
+        OptimalStrategy optimalStrategy = new OptimalStrategy();
+        optimalStrategy.buildMatrix();
     }
 }
