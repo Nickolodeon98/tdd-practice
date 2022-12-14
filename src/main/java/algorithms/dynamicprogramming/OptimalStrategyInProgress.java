d1ba7ed8ceca6024e7e1347e6ef43e830813832e
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-14 11:17:29 +0900

    feat: 최적 전략 풀이 중 i 자릿수부터 j 자릿수 범위 숫자의 왼쪽과 오른쪽 맨 끝 숫자들을 각 칸의 (i, j)에 넣는 로직 구현

--- src/main/java/algorithms/dynamicprogramming/OptimalStrategyInProgress.java
+++ src/main/java/algorithms/dynamicprogramming/OptimalStrategyInProgress.java
@@ -4,22 +4,86 @@ import java.util.Arrays;
 
 public class OptimalStrategyInProgress {
 
+    class State {
+        int mine;
+        int yours;
+
+        public State(int mine, int yours) {
+            this.mine = mine;
+            this.yours = yours;
+        }
+
+        @Override
+        public String toString() {
+            return "(" +
+                    mine +
+                    ", " + yours +
+                    ')';
+        }
+    }
+
+    public void storeEdges() {
+        int[] columns = {2, 7, 40, 19};
+
+        State[][] cache = new State[columns.length][columns.length];
+
+        for (int i = 0; i < cache.length; i++) {
+            for (int j = i; j < cache[i].length; j++) {
+                if (j == i) {
+                    cache[i][j] = new State(columns[i], 0);
+                    System.out.println(cache[i][j]);
+                    continue;
+                }
+                cache[i][j] = new State(columns[i], columns[j]);
+                System.out.println(cache[i][j]);
+            }
+        }
+
+//        for (int i = 1; i < cache.length; i++) { // 행
+//            for (int j = 0; j < cache[i].length; j++) { // 열
+//                int left = columns[j];
+//                int right = columns[j+i];
+//                cache[i][j] = new State(left, right);
+//                System.out.println(cache[i][j]);
+//            }
+//        }
+
+//        for (State[] states : cache) {
+//            System.out.println(Arrays.toString(states));
+//        }
+    }
+
+    public void dummyMethod() {
+        int[] columns = {2, 7, 40, 19};
+
+        State[][] cache = new State[columns.length][columns.length];
+
+        for (int i = 1; i < cache.length; i++) { // 행
+            for (int j = 0; j < cache[i].length - i; j++) { // 열
+                int left = columns[j];
+                int right = columns[j+i];
+                cache[i][j] = new State(left, right);
+                System.out.println(cache[i][j]);
+            }
+        }
+
+//        for (State[] states : cache) {
+//            System.out.println(Arrays.toString(states));
+//        }
+    }
+
     public static void fillMatrix() {
         int[] columns = {2, 7, 40, 19};
 
         int[][] memo = new int[columns.length][columns.length];
 
         for (int i = 0; i < memo.length; i++) {
-            for (int j = 0; j < memo[i].length; j++) {
-                if (i == j) {
+            for (int j = i; j < memo[i].length; j++) {
+                if (j == 0) {
                     memo[i][j] = columns[i];
                     continue;
                 }
-                if (i > j) {
-                    memo[i][j] = 0;
-                    continue;
-                }
-                memo[i][j] = memo[i][j-1] + columns[j];
+                memo[i][j] = memo[i][j - 1] + columns[j];
             }
         }
 
@@ -30,7 +94,12 @@ public class OptimalStrategyInProgress {
     }
 
     public static void main(String[] args) {
-        fillMatrix();
+//        fillMatrix();
+
+        OptimalStrategyInProgress optimalStrategyInProgress = new OptimalStrategyInProgress();
+        optimalStrategyInProgress.storeEdges();
+        System.out.println("----------------");
+        optimalStrategyInProgress.dummyMethod();
     }
 
 }
