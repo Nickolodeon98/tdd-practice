504a16a932be4c36face1298f224fcbccd3650c4
Nickolodeon98 <sjeon0730@gmail.com>
2023-04-07 07:21:00 +0900

    feat: 삼성 기출문제 1 풀이 중 2

--- src/main/java/algorithms/samsung/CodeTreeBread.java
+++ src/main/java/algorithms/samsung/CodeTreeBread.java
@@ -1,5 +1,7 @@
 package algorithms.samsung;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class CodeTreeBread {
@@ -9,6 +11,9 @@ public class CodeTreeBread {
   public int[][] web = new int[n][];
 
   int[][] convenience = new int[n * n][2];
+  int[][] dist = new int[n][];
+
+  int[][] dir = {{0, -1}, {-1, 0}, {1, 0}, {0, 1}};
 
   // 사람
   class Person {
@@ -18,6 +23,8 @@ public class CodeTreeBread {
     int goalX;
     int goalY;
   }
+
+  Person[] people = new Person[31];
 
   public void readInput() {
     Scanner sc = new Scanner(System.in);
@@ -38,41 +45,120 @@ public class CodeTreeBread {
       y = 0;
     }
 
-    for (int i = 0; i < convenience.length; i++) {
+
+    for (int i = 1; i < people.length; i++) {
       String positions = sc.nextLine();
-      convenience[i][0] = Integer.parseInt(positions.split(" ")[0]);
-      convenience[i][1] = Integer.parseInt(positions.split(" ")[1]);
+      people[i].goalX = Integer.parseInt(positions.split(" ")[0]);
+      people[i].goalY = Integer.parseInt(positions.split(" ")[1]);
     }
+
+
   }
 
-  public void process(Person[] people) {
+  public boolean isInRange(int x, int y) {
+    return !(x > n || x < 0 || y > n || y < 0);
+  }
+
+  public int process() {
+    int count = 1;
+
     while (!isFinished()) {
       //    Step 1: 1칸 움직임
       for (Person person : people) {
-        if (person.currentX != person.goalX || person.currentY != person.goalY) {
-          movePerson(person);
-        }
-        //    Step 2: 도착 여부 확인
-        if (checkArrived(person)) {
-          web[person.currentX][person.currentY] = -1;
-        }
-        //    Step 3: 출발
-        BFS(web);
-        initiate(person);
+        if (!isArrived(person)) movePerson(person);
       }
 
+      //    Step 2: 도착 여부 확인
+      for (Person person : people) {
+        checkArrived(person);
+      }
+
+      //    Step 3: 사람이 출발하는 곳 -> 베이스캠프 들어간다
+      initiate(people[count]);
+
+      count++;
     }
 
-
+    return count;
   }
 
-  public boolean checkArrived(Person person) {
+  private void initiate(Person person) {
+
+//    BFS(x, y);
+    // 모든 거리 중 최단거리를 찾는다.
+    int minDist = 10000;
+    int x = 0, y = 0;
+    for (int k = 0; k < dist.length; k++) {
+      for (int d : dist[k]) {
+        if (minDist > d) {
+          minDist = d;
+          x = k;
+          y = d;
+        }
+      }
+    }
+    person.currentX = x;
+    person.currentY = y;
+    web[x][y] = -1;
+  }
+
+  private void BFS(int x, int y, int[] goal, int[][] map) {
+    // 베이스캠프로부터 모든 격자까지의 최단 거리를 구한다.
+    Queue<int[]> q = new LinkedList<>();
+    boolean[][] visited = new boolean[n][n];
+    int[] start = {x, y};
+    q.add(start);
+    while(!q.isEmpty()) {
+      int[] rowAndColumn = q.poll();
+      visited[rowAndColumn[0]][rowAndColumn[1]] = true;
+
+      for (int i = 0; i < dir.length; i++) {
+        if (!isInRange(x + dir[i][0], y + dir[i][1])) continue;
+        if (visited[x + dir[i][0]][y + dir[i][1]]) continue;
+        int[] toAdd = {x + dir[i][0], y + dir[i][1]};
+        q.add(toAdd);
+        visited[x + dir[i][0]][y + dir[i][1]] = true;
+      }
+
+
+      }
+    }
+  }
+
+  private void movePerson(Person person) {
+    // 목표 편의점으로부터 사람 현재 위치의 사방까지의 거리를 구한다
+//    BFS();
+    int minDist = 10000;
+    int x = 0, y = 0;
+    // 최단거리를 구한다
+    for (int m = 0; m < 4; m++) {
+      if (minDist > dist[person.currentX + dir[m][0]][person.currentY + dir[m][1]]) {
+        minDist = dist[person.currentX + dir[m][0]][person.currentY + dir[m][1]];
+        x = person.currentX + dir[m][0];
+        y = person.currentY + dir[m][1];
+      }
+    }
+
+    person.currentX = x;
+    person.currentY = y;
+  }
+
+  public void checkArrived(Person person) {
+    if (isArrived(person)) {
+      web[person.currentX][person.currentY] = -1;
+    }
+  }
+
+  private boolean isArrived(Person person) {
     return person.currentX == person.goalX && person.currentY == person.goalY;
   }
 
   public boolean isFinished() {
-    for (int j = 0; )
-    if (convenience[j][0])
+    for (Person person : people) {
+      if (!isArrived(person))
+        return false;
+    }
+    return true;
   }
 
 
