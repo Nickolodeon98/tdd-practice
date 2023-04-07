2ffce8fa7f2a4aeefdf377a79c21578f2070982c
Nickolodeon98 <sjeon0730@gmail.com>
2023-04-07 20:57:41 +0900

    feat: 삼성 기출문제 1 풀이 중 3

--- src/main/java/algorithms/samsung/CodeTreeBread.java
+++ src/main/java/algorithms/samsung/CodeTreeBread.java
@@ -8,158 +8,279 @@ public class CodeTreeBread {
 
   //  격자
   public int n, m;
-  public int[][] web = new int[n][];
 
-  int[][] convenience = new int[n * n][2];
-  int[][] dist = new int[n][];
+  public void setN(int n) {
+    this.n = n;
+  }
+
+  public void setM(int m) {
+    this.m = m;
+  }
+
+  public int[][] web = new int[20][20];
+
+//  int[][] dist = new int[20][20];
 
   int[][] dir = {{0, -1}, {-1, 0}, {1, 0}, {0, 1}};
 
   // 사람
   class Person {
 
+    public Person() {
+      this.currentX = 0;
+      this.currentY = 0;
+      this.goalX = 0;
+      this.goalY = 0;
+    }
+
     int currentX;
     int currentY;
     int goalX;
     int goalY;
+
+    public void setCurrentX(int currentX) {
+      this.currentX = currentX;
+    }
+
+    public void setCurrentY(int currentY) {
+      this.currentY = currentY;
+    }
+
+    public Person(int currentX, int currentY, int goalX, int goalY) {
+      this.currentX = currentX;
+      this.currentY = currentY;
+      this.goalX = goalX;
+      this.goalY = goalY;
+    }
   }
 
   Person[] people = new Person[31];
 
-  public void readInput() {
+  public Person[] readInput() {
     Scanner sc = new Scanner(System.in);
 
     String info = sc.nextLine();
     String[] nAndM = info.split(" ");
 
-    n = Integer.parseInt(nAndM[0]);
-    m = Integer.parseInt(nAndM[1]);
+    setN(Integer.parseInt(nAndM[0]));
+    setM(Integer.parseInt(nAndM[1]));
 
-    int y = 0;
-    for (int x = 0; x < web.length; x++) {
-      String[] coordinates = sc.nextLine().split(" ");
-      while (y < coordinates.length) {
-        web[x][y] = Integer.parseInt(coordinates[y]);
+    for (int num = 1; num <= m; num++) {
+      people[num] = new Person();
+    }
+    String[] coordinates = new String[n];
+
+    int y = 1;
+    for (int x = 1; x <= n; x++) {
+      coordinates = sc.nextLine().split(" ");
+
+      while (y <= coordinates.length) {
+//        System.out.println(coordinates[y-1]);
+        web[x][y] = Integer.parseInt(coordinates[y-1]);
         y++;
       }
-      y = 0;
+//      for (String s : coordinates) {
+//        System.out.print(s + " ");
+//      }
+//      System.out.println();
+      y = 1;
     }
 
 
-    for (int i = 1; i < people.length; i++) {
+    for (int i = 1; i <= m; i++) {
       String positions = sc.nextLine();
       people[i].goalX = Integer.parseInt(positions.split(" ")[0]);
       people[i].goalY = Integer.parseInt(positions.split(" ")[1]);
     }
 
-
+    return people;
   }
 
   public boolean isInRange(int x, int y) {
-    return !(x > n || x < 0 || y > n || y < 0);
+    return x <= n && x >= 1 && y <= n && y >= 1;
   }
 
   public int process() {
-    int count = 1;
+    Person[] people = readInput();
 
-    while (!isFinished()) {
+    System.out.println("memoization: " + m);
+//    for (int[] row : web) {
+//      for (int column : row)
+//        System.out.print(column + " ");
+//      System.out.println();
+//    }
+
+    for (int i = 1; i <= m; i++) {
+//      System.out.println("initialGoalX: " + people[i].goalX);
+//      System.out.println("initialCurrentX: " + people[i].currentX);
+//      System.out.println("initialGoalY: " + people[i].goalY);
+//      System.out.println("initialCurrentY: " + people[i].currentY);
+    }
+
+    int count = 0;
+
+    while (!isFinished(people)) {
+      count++;
+
       //    Step 1: 1칸 움직임
-      for (Person person : people) {
-        if (!isArrived(person)) movePerson(person);
+      for (int i = 1; i < count && i <= m; i++) {
+        if (!isArrived(people[i])) {
+          Person person = movePerson(people[i]);
+          people[i] = new Person(person.currentX, person.currentY, person.goalX, person.goalY);
+//          System.out.println("cX: " + people[i].currentX + "m: " + m);
+//          System.out.println("gX: " + people[i].goalX);
+//          System.out.println("cY: " + people[i].currentY);
+//          System.out.println("gY: " + people[i].goalY);
+        }
       }
 
       //    Step 2: 도착 여부 확인
-      for (Person person : people) {
-        checkArrived(person);
+      for (int j = 1; j < count && j <= m; j++) {
+        web = checkArrived(people[j], web);
       }
 
       //    Step 3: 사람이 출발하는 곳 -> 베이스캠프 들어간다
-      initiate(people[count]);
-
-      count++;
+      if (m >= count) {
+        System.out.println("hereherehere");
+        int[] xAndY = initiate(people[count], web);
+        people[count].setCurrentX(xAndY[0]);
+        System.out.println("toChangeX: " + xAndY[0]);
+        System.out.println("changedX: " + people[count].currentX);
+        people[count].setCurrentY(xAndY[1]);
+        System.out.println("changedY: " + people[count].currentY);
+        System.out.println("toChangeY: " + xAndY[1]);
+        web[xAndY[0]][xAndY[1]] = -1;
+      }
+//      System.out.println("Count: " + count);
     }
 
     return count;
   }
 
-  private void initiate(Person person) {
+  private int[] initiate(Person person, int[][] web) {
 
-//    BFS(x, y);
+    int[][] dist = BFS(person.currentX, person.currentY);
     // 모든 거리 중 최단거리를 찾는다.
     int minDist = 10000;
-    int x = 0, y = 0;
-    for (int k = 0; k < dist.length; k++) {
-      for (int d : dist[k]) {
-        if (minDist > d) {
-          minDist = d;
-          x = k;
-          y = d;
+    int minX = 0, minY = 0;
+    for (int k = 1; k <= n; k++) {
+      for (int l = 1; l <= n; l++) {
+        if (web[k][l] != 1) {
+
+          continue;
+        }
+        if (minDist > dist[k][l]) {
+          minDist = dist[k][l];
+          minX = k;
+          minY = l;
         }
       }
     }
-    person.currentX = x;
-    person.currentY = y;
-    web[x][y] = -1;
+    return new int[]{minX, minY};
   }
 
-  private void BFS(int x, int y, int[] goal, int[][] map) {
+  private int[][] BFS(int x, int y) {
     // 베이스캠프로부터 모든 격자까지의 최단 거리를 구한다.
-    Queue<int[]> q = new LinkedList<>();
-    boolean[][] visited = new boolean[n][n];
-    int[] start = {x, y};
-    q.add(start);
-    while(!q.isEmpty()) {
-      int[] rowAndColumn = q.poll();
-      visited[rowAndColumn[0]][rowAndColumn[1]] = true;
+    Queue<Integer> q = new LinkedList<>();
+    int[][] dist = new int[20][20];
 
-      for (int i = 0; i < dir.length; i++) {
-        if (!isInRange(x + dir[i][0], y + dir[i][1])) continue;
-        if (visited[x + dir[i][0]][y + dir[i][1]]) continue;
-        int[] toAdd = {x + dir[i][0], y + dir[i][1]};
-        q.add(toAdd);
-        visited[x + dir[i][0]][y + dir[i][1]] = true;
-      }
-
-
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
+        dist[i][j] = 10000;
       }
     }
+
+    q.add(x);
+    q.add(y);
+    dist[x][y] = 0;
+
+    while (!q.isEmpty()) {
+      int row = q.poll();
+      int column = q.poll();
+      for (int i = 0; i < dir.length; i++) {
+        int nx = row + dir[i][0], ny = column + dir[i][1];
+        if (!isInRange(nx, ny)) {
+          continue;
+        }
+        if (dist[nx][ny] != 10000) {
+          continue;
+        }
+        if (web[nx][ny] == -1) {
+          continue;
+        }
+        q.add(nx);
+        q.add(ny);
+        dist[nx][ny] = dist[row][column] + 1;
+      }
+    }
+//    for (int[] row : dist) {
+//      for (int column : row)
+//        System.out.print(" " + column);
+//      System.out.println();
+//    }
+
+    return dist;
   }
 
-  private void movePerson(Person person) {
+  private Person movePerson(Person person) {
     // 목표 편의점으로부터 사람 현재 위치의 사방까지의 거리를 구한다
-//    BFS();
+    int[][] dist = BFS(person.goalX, person.goalY);
     int minDist = 10000;
-    int x = 0, y = 0;
+    int minX = 0, minY = 0;
     // 최단거리를 구한다
     for (int m = 0; m < 4; m++) {
-      if (minDist > dist[person.currentX + dir[m][0]][person.currentY + dir[m][1]]) {
-        minDist = dist[person.currentX + dir[m][0]][person.currentY + dir[m][1]];
-        x = person.currentX + dir[m][0];
-        y = person.currentY + dir[m][1];
+      int px = person.currentX + dir[m][0], py = person.currentY + dir[m][1];
+      if (!isInRange(px, py)) {
+//        System.out.println("pass");
+        continue;
+      }
+      if (minDist > dist[px][py]) {
+        System.out.println("update");
+        minDist = dist[px][py];
+        minX = px;
+        minY = py;
       }
     }
 
-    person.currentX = x;
-    person.currentY = y;
+//    System.out.println("minDist: " + minDist);
+//    System.out.println("minX: " + minX);
+//    System.out.println("minY: " + minY);
+
+    person.setCurrentX(minX);
+    person.setCurrentY(minY);
+    return person;
   }
 
-  public void checkArrived(Person person) {
+  public int[][] checkArrived(Person person, int[][] web) {
     if (isArrived(person)) {
       web[person.currentX][person.currentY] = -1;
     }
+
+    return web;
   }
 
   private boolean isArrived(Person person) {
+//    System.out.println("currentX: " + person.currentX);
+//    System.out.println("goalX: " + person.goalX);
+//    System.out.println("currentY: " + person.currentY);
+//    System.out.println("goalY: " + person.goalY);
     return person.currentX == person.goalX && person.currentY == person.goalY;
   }
 
-  public boolean isFinished() {
-    for (Person person : people) {
-      if (!isArrived(person))
+  public boolean isFinished(Person[] people) {
+    for (int i = 1; i <= m; i++) {
+      if (!isArrived(people[i])) {
+//        System.out.println("hello");
         return false;
+      }
     }
     return true;
   }
 
+  public static void main(String[] args) {
+    CodeTreeBread solution = new CodeTreeBread();
+    int answer = solution.process();
+    System.out.println(answer);
+  }
 
 }
