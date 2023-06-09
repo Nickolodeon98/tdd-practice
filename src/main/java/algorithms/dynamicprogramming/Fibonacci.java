67e83bd033708218ec2164c1477b899c3edad8e8
Nickolodeon98 <sjeon0730@gmail.com>
2023-06-09 22:41:13 +0900

    feat: 피보나치 수열

--- src/main/java/algorithms/dynamicprogramming/Fibonacci.java
+++ src/main/java/algorithms/dynamicprogramming/Fibonacci.java
@@ -7,7 +7,7 @@ public class Fibonacci {
     /* n = 5 일 때, memo[5] = fibo(4) + fibo(3) -> 여기서는 memo[3] 만 리턴된다.
      * fibo(4) = memo[4] = fibo(3) + fibo(2) // 깊이 들어간다.
      * memo[3] = 2 + 1 = 3; // memo[3] 이 정해졌다.
-     * 궁금한 점: 정수 배열은 원래 변동이 적용이 안되지 않나요?
+     * 궁금한 점: 정수 배열은 원래 변동이 적용이 안되지 않나요? 답: 됩니다.
      * */
     public int fibonacciWithMemo(int n, int[] memo) {
         if (n == 0) throw new RuntimeException();
