3afaa2dc31181c037d78fb6fd8305b87284e96fa
Nickolodeon98 <sjeon0730@gmail.com>
2023-08-03 18:22:47 +0900

    feat: 프로그래머스 실패율 주석 제거

--- src/main/java/algorithms/programmers/P42889.java
+++ src/main/java/algorithms/programmers/P42889.java
@@ -3,8 +3,6 @@ package algorithms.programmers;
 import java.util.*;
 
 public class P42889 {
-
-
   public List<Integer> stageCnt(int N, int[] stages) {
     int ptr = 1;
     int cnt = 0;
@@ -12,9 +10,6 @@ public class P42889 {
 
     Arrays.sort(stages);
 
-    // for (int j = 0; j < stages.length; j++) {
-    //     System.out.println(stages[j]);
-    // }
     List<Double> percents = new ArrayList<>();
     int i = stages.length - 1;
     while (i >= 0 && ptr <= N) {
@@ -56,10 +51,6 @@ public class P42889 {
       count++;
     }
 
-    // for (Map.Entry<Integer, Double> e : forSort.entrySet()) {
-    //     System.out.println(e.getKey() + " : " + e.getValue());
-    // }
-
     List<Integer> orders = new ArrayList<>(forSort.keySet());
     Collections.sort(orders, (o1, o2) -> Double.compare(forSort.get(o2), forSort.get(o1)));
 
@@ -67,14 +58,7 @@ public class P42889 {
   }
 
   public List<Integer> solution(int N, int[] stages) {
-    // int[] answer = new int[N];
-    // List<Integer> solved = stageCnt(N, stages);
-    // for (int i = 0; i < answer.length; i++) {
-    //     answer[i] = solved.get(i);
-    // }
-    // return answer;
     return stageCnt(N, stages);
-    // return stageCnt(N, stages).stream().mapToInt(Integer::intValue).toArray();
   }
 
 }
