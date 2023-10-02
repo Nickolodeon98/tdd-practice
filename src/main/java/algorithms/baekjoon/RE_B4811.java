c20419482ab51255894fac9663056e2f25e1ae2d
Nickolodeon98 <sjeon0730@gmail.com>
2023-10-02 23:58:04 +0900

    feat: 백준 알약 풀이 중

---
+++ src/main/java/algorithms/baekjoon/RE_B4811.java
@@ -0,0 +1,27 @@
+package algorithms.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RE_B4811 {
+    static int N = 0;
+    static int pills = 0;
+    public static void solution() {
+
+
+    }
+
+    public static void countCases() {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while ((N = Integer.valueOf(br.readLine())) != 0) {
+            pills = 2 * N;
+
+        }
+    }
+}
