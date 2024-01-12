586198981ee8842fbf32f51a63b57824aefe049c
DESKTOP-26MNF15\shJeon <sh-jeon@pntbiz.com>
2024-01-12 19:33:12 +0900

    feat: 마인크래프트 풀이 중

--- src/main/java/algorithms/baekjoon/B18111.java
+++ src/main/java/algorithms/baekjoon/B18111.java
@@ -2,4 +2,13 @@ package algorithms.baekjoon;
 
 public class B18111 {
 
+    private static void mine() {
+
+        return;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
 }
