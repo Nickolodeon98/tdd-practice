020799b23c8586bb44fc8b8ddcdf458a517d5fe3
DESKTOP-26MNF15\shJeon <sh-jeon@pntbiz.com>
2024-01-12 19:36:10 +0900

    feat: 마인크래프트 풀이 업데이트

--- src/main/java/algorithms/baekjoon/B18111.java
+++ src/main/java/algorithms/baekjoon/B18111.java
@@ -1,14 +1,25 @@
 package algorithms.baekjoon;
 
-public class B18111 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-    private static void mine() {
+public class B18111 {
+    static boolean[] visited;
+    private static void DFS() {
+        for (int i = 0; i < 20; i++) {
+            if (visited[i]) break;
+            DFS();
+        }
 
         return;
     }
 
-    public static void main(String[] args) {
-        System.out.println();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        visited = new boolean[20];
+        String s = br.readLine();
+        System.out.println(s);
     }
 
 }
