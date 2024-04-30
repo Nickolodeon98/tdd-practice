4857a36237c66295d63fff44ec92aa86763bb8f9
Nickolodeon98 <sjeon0730@gmail.com>
2024-04-30 22:19:47 +0900

    feat: 프로그래머스 체육복 2번째 푸는 중 - 풀이 완료

--- src/main/java/algorithms/programmers/P42862_2.java
+++ src/main/java/algorithms/programmers/P42862_2.java
@@ -11,9 +11,14 @@ public class P42862_2 {
     Set<Integer> availables = new TreeSet<>();
     Set<Integer> reserved = new TreeSet<>();
 
+    for (int i = 0; i < record.length; i++) {
+      record[i] = -2;
+    }
+
     Arrays.stream(lost).forEach(i -> {
       availables.add(i);
       everyone.add(i);
+      record[i] = 0;
     }); // 먼저 잃어버린 체육복의 목록을 저장한다.
 
     System.out.println("잃어버린 체육복 목록: " + availables);
@@ -21,6 +26,7 @@ public class P42862_2 {
     Arrays.stream(reserve).forEach(i -> {
       reserved.add(i);
       everyone.add(i);
+      record[i] = -2;
     }); // 여벌 옷이 있는 학생들을 저장한다.
 
     System.out.println("여벌 옷이 있는 학생들 목록: " + reserved);
@@ -47,6 +53,7 @@ public class P42862_2 {
       if (record[i] == 1) {
         remained.remove(i);
         availables.remove(i);
+        record[i] = -1;
         count++;
       }
     }
@@ -55,25 +62,33 @@ public class P42862_2 {
     // 잃어버린 옷도 해결되어서 사라진다.
     remained.forEach(i -> {
           // 여벌이 있으나 도난당한 인원이 아니면, 빌려준다.
-          if (record[i] != 1 && availables.remove(i - 1) == true) {
+          if (record[i] != 1 && record[i] != -1 && availables.remove(i - 1) == true) {
             System.out.println("지웁니다: " + i);
             record[i] = 1;
+            record[i - 1] = 3;
+          } else if (record[i] != 1 && record[i] != -1 && availables.remove(i + 1) == true) {
+            System.out.println("지웁니다: " + i);
+            record[i] = 1;
+            record[i - 1] = 3;
           }
         }
     );
 
     System.out.println("count : " + count);
 
-    System.out.println("뒷 사람들한테 빌려주니까 남은 옷들 목록: " + remained);
+    System.out.println("사람들한테 빌려주니까 남은 옷들 목록: " + Arrays.toString(record));
 
     // 다시 한 번, 잃어버린 체육복에서 현재 여벌 옷으로부터 빌릴 수 있는지 확인해본다.
     // 빌렸으면, availables 에는 잃어버린 옷들이 매우 적어지게 된다.
 
-    remained.stream().forEach(i -> {
-      if (record[i] != 1) {
-        availables.remove(i + 1);
-      }
-    });
+    // remained.stream().forEach(i -> {
+    //     if (record[i] != 1 && record[i] != -1 && availables.remove(i+1) == true) {
+    //         record[i] = 1;
+    //         record[i+1] = 3;
+    //     }
+    // });
+
+    // System.out.println("앞 사람들한테 빌려주니까 남은 옷들 목록: " + Arrays.toString(record));
     // 가장 처음에 잃어버린 옷들의 개수에서 얼마나 빠졌는지 확인한다
     int diff = (int) Math.abs(counts - (availables.size() + count));
 
@@ -82,7 +97,17 @@ public class P42862_2 {
 
     // 빠진 만큼은 체육복이 생긴 것이므로
     // (현재) 여벌 체육복 개수 + 체육복이 생긴 사람 수 + 원래 있던 사람의 수를 합치면 결과값이다.
+    int c = 0;
+    for (int i = 0; i < record.length; i++) {
+      if (record[i] == 1) {
+        c++;
+      }
+    }
+
     answer = remained.size() + diff + (n - everyone.size()) + count;
+    // for (int i = 1; i < record.length; i++) {
+    //     if (record[i] != 0) answer++;
+    // }
 
     System.out.println("찐으로 남은 여벌 옷이 있는 학생들 수 (" + remained.size() + ") + 옷을 빌린 학생 수 (" + diff
         + ") + 여벌 옷이 있지도 않고 옷을 빌리지도 않은 평범한 학생들 수 (" + (n - everyone.size())
