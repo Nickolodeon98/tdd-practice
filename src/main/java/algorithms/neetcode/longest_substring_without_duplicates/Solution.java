1e9f7bf553d0509aebb99df93b4585d54fec782f
Nickolodeon98 <sjeon0730@gmail.com>
2024-10-07 21:59:20 +0900

    feat: neetcode length of longest substring without duplicates 풀이 완료

--- src/main/java/algorithms/neetcode/longest_substring_without_duplicates/Solution.java
+++ src/main/java/algorithms/neetcode/longest_substring_without_duplicates/Solution.java
@@ -1,17 +1,39 @@
 package algorithms.neetcode.longest_substring_without_duplicates;
 
-public class Solution {
-
+class Solution {
   public int lengthOfLongestSubstring(String s) {
-    int idx = 0;
-
-    for () {
-
+    // 해 - globalMax
+    // 매번 가장 긴 부분 문자열 - localMax
+    if (s.equals("")) {
+      return 0;
     }
+
+    char[] str = s.toCharArray();
+
+    String globalMax = String.valueOf(str[0]);
+    System.out.println(globalMax);
+    String localMax = globalMax;
+
+    for (int idx = 1; idx < str.length; idx++) {
+      // 중복이 있을 시, localMax을 초기화한다.
+      String current = String.valueOf(str[idx]);
+      while (localMax.contains(current)) {
+        if (String.valueOf(localMax.charAt(0)).equals(current)) {
+          localMax = localMax.substring(1);
+        } else {
+          localMax = "";
+          break;
+        }
+      }
+
+      localMax += str[idx];
+      System.out.println(localMax);
+
+      if (globalMax.length() < localMax.length()) {
+        globalMax = localMax;
+      }
+    }
+
+    return globalMax.length();
   }
-
-  public static void main(String[] args) {
-
-  }
-
 }
