d9317523e22f49ed9ccfe158b1c11a165a7f46d4
Nickolodeon98 <sjeon0730@gmail.com>
2024-11-25 23:35:09 +0900

    feat: neetcode longest repeating character replacement 풀이 완료

--- src/main/java/algorithms/neetcode/longest_repeating_character_replacement/Solution.java
+++ src/main/java/algorithms/neetcode/longest_repeating_character_replacement/Solution.java
@@ -1,41 +1,65 @@
 package algorithms.neetcode.longest_repeating_character_replacement;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 class Solution {
+
   public int characterReplacement(String s, int k) {
+
     // Map 에 key 는 알파벳으로, value 는 개수로 넣은 후 value 로 내림차순
-    Map<String, Integer> count = new HashMap<>();
-    int start = 0;
-    int end = s.length() - 1;
 
-    // 문자열의 시작 부분이 끝 부분과 다르다면, end -= 1 을 한다.
-    while (s.charAt(start) != s.charAt(end) && end > 0) {
-      end--;
+    Map<Character, Integer> alphabets = new TreeMap<>();
+    int n = 0;
+    for (char c = 'A'; c <= 'Z'; c++) {
+      alphabets.put(c, n++);
     }
+    List<Integer> nums = new ArrayList<>();
 
-    // 현재 두 문자의 값을 하나의 변수 letter 에 저장한다.
-    char letter = s.charAt(end);
-    System.out.println("letter: " + letter);
+    int[] letters = new int[26];
 
-    // 문자열과 시작과 끝 부분이 같아진다면, substring 으로 자른다. end 는 기억해둔다.
-    String sub = s.substring(start, end+1);
-    System.out.println("sub: " + sub);
-    int tempStart = start;
-    // start++; 한다.
-    while (k > 0 && start < sub.length()) {
-      // k > 0 이라면, letter 로 바꿔치기 한다.
-      if (sub.charAt(start) != letter) {
-        sub.replace(String.valueOf(sub.charAt(start)), String.valueOf(letter));
-        k--;
+    int start = 0;
+    int i = start;
+    int end = start;
+    String curStr = "";
+    // start ~ end 문자열
+    while (start <= end && end < s.length()) {
+      System.out.println("start: " + start + ", " + "end: " + end);
+      curStr = s.substring(start, end + 1);
+      for (int idx = start; idx <= end; idx++) {
+        letters[alphabets.get(curStr.charAt(idx - start))] += 1;
+      }
+      System.out.println(Arrays.toString(letters));
+      int max = -1;
+      int maxIdx = 0;
+      for (int idx = 0; idx < letters.length; idx++) {
+        if (max < letters[idx]) {
+          max = letters[idx];
+          maxIdx = idx;
+        }
+      }
+      letters = new int[26];
+
+      if (curStr.length() - max <= k) {
+        System.out.println(curStr.length() - max);
+        if (curStr.length() - max == k) {
+          nums.add(curStr.length());
+        }
+        end++;
+        continue;
       }
       start++;
     }
-    System.out.println("k: " + k);
-    System.out.println("start: " + start);
-    // System.out.println("result: " + sub.substring(tempStart, start+1));
-    // k = 0 이라면, 현재 start++ 후 ~ end 까지 문자열을 저장
-    return start == sub.length() ? sub.substring(tempStart, start).length() : sub.substring(tempStart, start+1).length();
+    nums.add(curStr.length());
+
+    Collections.sort(nums, (a, b) -> b - a);
+
+    System.out.println(nums);
+
+    return nums.get(0);
   }
 }
