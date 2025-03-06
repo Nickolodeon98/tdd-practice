856c804cd82d05c5d55abcc86457f05abc517614
Nickolodeon98 <sjeon0730@gmail.com>
2025-03-06 21:36:52 +0900

    feat: neetcode same tree 풀이 완료

---
+++ src/main/java/algorithms/neetcode/same_tree/Solution.java
@@ -0,0 +1,40 @@
+package algorithms.neetcode.same_tree;
+
+class Solution {
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null) {
+      if (q == null) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    if (q == null) {
+      if (p == null) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    if (p.val != q.val) {
+      return false;
+    }
+    return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+  }
+
+  static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+}
+
